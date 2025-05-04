bf08d1621ac6713215404879d2a387d25cdfd855
Guilherme Gonçalves <guilhermee9@live.com>
2025-05-04 16:23:58 -0300

    atualizado

---
+++ js/main.js
@@ -0,0 +1,11 @@
+<script>
+    function toggleMenu() {
+    var sidebar = document.getElementById("mySidebar");
+    sidebar.classList.toggle("active");
+}
+
+    function closeMenu() {
+    var sidebar = document.getElementById("mySidebar");
+    sidebar.classList.remove("active");
+}
+</script>
